adc5429f815452dc7e1f7a5065fdb0a29681f306
Kateryna Gladka <pi60_gkv@student.ztu.edu.ua>
2022-11-07 19:18:47 +0200

    created file task3_1.php in which created form

---
+++ task3_1.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Вивидення елементів масиву, що повторюються</title>
+</head>
+<body>
+<form method="post">
+    <label for="array">Введіть елементи масиву через кому</label>
+    <input type="text" id="array" name="array" value="1, 2, 3, 4, 1, 2">
+    <button>Отримати</button>
+</form>
+</body>
+</html>
