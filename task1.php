0abc123665885ddce69dcf017997abfb3f1699fc
Kateryna Gladka <pi60_gkv@student.ztu.edu.ua>
2022-11-07 16:39:14 +0200

    Created task1.php files, in which created table and form. Created task1.css and created style in him.

---
+++ task1.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Робота з функціями</title>
+    <link rel="stylesheet" href="task1.css">
+</head>
+<body>
+<table>
+    <tr>
+        <th>x<sup>y</sup></th>
+        <th>x!</th>
+        <th>my_tg(x)</th>
+        <th>sin(x)</th>
+        <th>cos(x)</th>
+        <th>tg(x)</th>
+        <th>x+y</th>
+        <th>x-y</th>
+        <th>x*y</th>
+        <th>x/y</th>
+        <th>average(x,y)</th>
+    </tr>
+    <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+    </tr>
+</table>
+
+<form method="post">
+    <div>
+        <label for="field1">x</label>
+        <label for="field2">y</label>
+    </div>
+    <input type="text" id="field1" name="field1" value="1">
+    <input type="text" id="field2" name="field2" value="1">
+    <button type="submit">=</button>
+</form>
+</body>
+</html>
